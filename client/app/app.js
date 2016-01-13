78b0ee56c21582506e7e46605717ae4a36c0fde9
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-13 00:52:49 -0200

    added runApp function to angular run

--- client/app/app.js
+++ client/app/app.js
@@ -2,6 +2,28 @@
     /* global angular */
     'use strict';
 
+    var runApp = function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            event.targetScope.$watch('$viewContentLoaded', function () {
+                angular.element('html, body, #content').animate({ scrollTop: 0 }, 200);
+
+                setTimeout(function () {
+                    angular.element('#wrap').css('visibility', 'visible');
+
+                    if (!angular.element('.dropdown').hasClass('open')) {
+                        angular.element('.dropdown').find('>ul').slideUp();
+                    }
+                }, 200);
+            });
+
+            $rootScope.containerClass = toState.containerClass;
+        });
+    };
+
+    runApp.$injector = ['$rootScope', '$state', '$stateParams'];
+
     var defaultRoute = function ($urlRouterProvider) {
         $urlRouterProvider.otherwise('/app/inicio');
     };
@@ -9,5 +31,6 @@
     defaultRoute.$injector = ['$urlRouterProvider'];
 
     angular.module('abelhas-operarias', ['ui.router'])
-        .config(defaultRoute);
+        .config(defaultRoute)
+        .run(runApp);
 })();
