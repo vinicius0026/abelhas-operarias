0fa8dbfee8e9ef31977bcba2723109d3d6b0d281
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-15 00:19:20 -0200

    setting correct db name in prod env

--- server/config/environment/prod.js
+++ server/config/environment/prod.js
@@ -6,7 +6,7 @@
 
 module.exports = {
     db: {
-        appDB: 'abelhas-operarias',
-        baseUrl: `mongodb://abelhasoperarias:${process.env.MONGOLAB_PASSWORD}@ds045795.mongolab.com:45795/heroku_9jp74hcm`
+        appDB: 'heroku_9jp74hcm',
+        baseUrl: `mongodb://abelhasoperarias:${process.env.MONGOLAB_PASSWORD}@ds045795.mongolab.com:45795/`
     }
 };
