19c52a566152f2a2f2d76591367f09803e293859
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-15 00:36:56 -0200

    allowing all origin, for testing purposes

--- server/config/express.js
+++ server/config/express.js
@@ -13,6 +13,7 @@ var bodyParser = require('body-parser'),
 
 module.exports = function (app) {
     app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS, PATCH, DELETE');
         next();
