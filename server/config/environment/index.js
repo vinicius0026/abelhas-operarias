0466b57ff84fe10ce06ba10255130ff3e7710d00
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-14 22:52:32 -0200

    created config files for multiple environments

---
+++ server/config/environment/index.js
@@ -0,0 +1,24 @@
+/**
+ * Environment Definitions
+ * In this file, the default configurations are set.
+ * For particular configurations of individual environments,
+ * see appropriate file (prod.js, dev.js, test.js) in this same
+ * directory.
+ */
+
+'use strict';
+
+var _ = require('lodash'),
+
+    all = {
+        env: process.env.NODE_ENV || 'dev',
+        port: 3000,
+        db: {
+            baseUrl: 'mongodb://localhost/'
+        },
+        secrets: {
+            session: process.env.APP_SECRET || 'fc7856d00b08ccb205f2ac1d6aada4d5'
+        }
+    };
+
+module.exports = _.merge(all, require('./' + process.env.NODE_ENV + '.js') || {});
