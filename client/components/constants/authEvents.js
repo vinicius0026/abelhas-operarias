feaf3c2f9f1f31d2910135d25783df0640e4c7a0
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-18 22:53:05 -0200

    added constants for auth events

---
+++ client/components/constants/authEvents.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    var authEvents = {
+        notAuthenticated: 'auth-not-authenticated',
+        notAuthorized: 'auth-not-authorized'
+    };
+
+
+    angular.module('constants-auth-events', [])
+        .constant('AUTH_EVENTS', authEvents);
+})();
