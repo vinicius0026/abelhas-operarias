9d917a3849708b1aec1d54c17f1294b9e7bb1392
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-19 22:20:15 -0200

    created constant for users api endpoint

--- client/components/constants/api.js
+++ client/components/constants/api.js
@@ -3,8 +3,21 @@
 
     'use strict';
 
-    var AUTH_URL = '/auth/local';
+    var API_BASE_URL = '/api',
+        AUTH_URL = '/auth/local',
+
+        usersUrls = (function () {
+            var base = API_BASE_URL.concat('/users');
+            return {
+                base: base,
+                fetch: base.concat('/fetch'),
+                one: function (id) {
+                    base.concat('/:id').replace(':id', id);
+                }
+            }
+        })();
 
     angular.module('abelhas-operarias')
-        .constant('AUTH_URL', AUTH_URL);
+        .constant('AUTH_URL', AUTH_URL)
+        .constant('USERS_URLS', usersUrls);
 })();
