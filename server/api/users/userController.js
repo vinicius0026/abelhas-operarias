07f010aa2e23f088408e4e2588e6e3c969c25467
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-19 22:58:21 -0200

    created user controller

---
+++ server/api/users/userController.js
@@ -0,0 +1,25 @@
+/**
+ * User API Controller
+ */
+
+var errorHandler = require('../../lib/util/errorHandler'),
+    User = require('./userModel'),
+
+    userController = {
+        create: function (req, res) {
+            User.spawn(req.body, (err, user) => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                res.status(201)
+                    .send({
+                        ok: true,
+                        info: `User created with id = ${user.id}`,
+                        data: user
+                    });
+            });
+        }
+    };
+
+module.exports = userController;
