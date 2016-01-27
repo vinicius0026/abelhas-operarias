eb181924bd1832ef3d82ded5c4ee9ba8b72b91c3
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-26 22:54:48 -0200

    adding get method to userService

--- client/app/users/usersService.js
+++ client/app/users/usersService.js
@@ -4,15 +4,24 @@
 
     var usersService = function (ApiRequests, USERS_URLS) {
         var create = function (data) {
-            return ApiRequests.req({
-                method: 'POST',
-                url: USERS_URLS.base,
-                data: data
-            });
-        };
+                return ApiRequests.req({
+                    method: 'POST',
+                    url: USERS_URLS.base,
+                    data: data
+                });
+            },
+
+            get = function (userId) {
+                console.log('USERS_URLS.one(userId)', USERS_URLS.one(userId));
+                return ApiRequests.req({
+                    method: 'GET',
+                    url: USERS_URLS.one(userId)
+                });
+            };
 
         return {
-            create: create
+            create: create,
+            get: get
         };
     };
 
