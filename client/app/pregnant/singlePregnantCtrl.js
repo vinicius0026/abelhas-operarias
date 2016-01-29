067b22bb1649e60f90be0f08fe046dfc83a94e30
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-29 20:49:40 -0200

    injecting pregnantService, creating controller methods to create and update pregnant

--- client/app/pregnant/singlePregnantCtrl.js
+++ client/app/pregnant/singlePregnantCtrl.js
@@ -3,7 +3,7 @@
 
     'use strict';
 
-    var SinglePregnantCtrl = function ($state, $stateParams, toastr) {
+    var SinglePregnantCtrl = function ($state, $stateParams, toastr, pregnantService) {
         var vm = this,
 
             validActions = ['cadastrar', 'visualizar', 'editar'],
@@ -17,15 +17,51 @@
 
             capitalizeFirstLetter = function (str) {
                 return str.charAt(0).toUpperCase() + str.slice(1);
+            },
+
+            createPregnant = function (data) {
+                pregnantService.create(data)
+                    .then(function () {
+                        toastr.success('Gestante cadastrada com sucesso.');
+                        $state.go('app.pregnant');
+                    }, function () {
+                        toastr.error('Erro ao cadastrar gestante. Favor tentar novamente');
+                    });
+            },
+
+            updatePregnant = function (data, pregnantId) {
+                pregnantService.update(pregnantId, data)
+                    .then(function () {
+                        toastr.success('Gestante atualizada com sucesso.');
+                        $state.go('app.pregnant.pregnant',
+                            {action: 'visualizar', id: vm.pregnant.id},
+                            {reload: true});
+                    }, function () {
+                        toastr.error('Erro ao atualizar dados da gestante');
+                    });
+            },
+
+            savePregnant = function (pregnantData) {
+                if (vm.form.$valid) {
+                    if (vm.action === 'cadastrar') {
+                        createPregnant(pregnantData);
+                    } else if (vm.action === 'editar') {
+                        updatePregnant(pregnantData, $stateParams.id);
+                    }
+                } else {
+                    toastr.warning('Preencha todos os campos obrigatórios.');
+                }
             };
 
         vm.capitalizeFirstLetter = capitalizeFirstLetter;
         vm.action = $stateParams.action;
+        vm.savePregnant = savePregnant;
 
         validateAction(vm.action);
     };
 
-    SinglePregnantCtrl.$inject = ['$state', '$stateParams', 'toastr'];
+    SinglePregnantCtrl.$inject = ['$state', '$stateParams', 'toastr',
+        'pregnantService'];
 
 
     angular.module('abelhas-operarias')
