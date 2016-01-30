c12feb7ba9437718935b896147977801db0dfb6c
Vinicius Teixeira <vinicius0026@gmail.com>
2016-01-29 23:52:45 -0200

    created pregnant model

---
+++ server/api/pregnant/pregnantModel.js
@@ -0,0 +1,54 @@
+/**
+ * Pregnant Model
+ */
+
+'use strict';
+
+var mask = require('json-mask'),
+    mongoose = require('mongoose'),
+
+    Pregnant,
+    Schema = mongoose.Schema,
+
+    pregnantCreateMask = 'name,cpf,occupation,age,spouse(name,occupation,age),' +
+        'numberOfChildren,ageOfChildren,babyGender,familyIncome,' +
+        'familyIncome,religion,education,phone,address,neighborhood,' +
+        'reference,ownHouse,rentValue,dateForDonation,referral',
+
+    pregnantMask = `id,${pregnantCreateMask}`,
+
+    PregnantSchema = new Schema({
+        name: {type: String, require: true, trim: true},
+        cpf: {type: String, index: {unique: true, dropDups: true}, trim: true},
+        age: {type: Number, min: 0},
+        occupation: {type: String, trim: true},
+        spouse: {
+            name: {type: String, trim: true},
+            occupation: {type: String, trim: true},
+            age: {type: Number, min: 0}
+        },
+        numberOfChildren: {type: Number, min: 0},
+        ageOfChildren: {type: String, trim: true},
+        babyGender: {type: String, enum: ['feminine', 'masculine']},
+        familyIncome: {type: Number, min: 0},
+        religion: {type: String, trim: true},
+        education: {type: String, trim: true},
+        phone: {type: String, trim: true},
+        address: {type: String, trim: true},
+        neighborhood: {type: String, trim: true},
+        reference: {type: String, trim: true},
+        ownHouse: Boolean,
+        rentValue: {type: Number, min: 0},
+        dateForDonation: {type: Date},
+        referral: {type: String, trim: true},
+        obs: {type: String, trim: true}
+    });
+
+PregnantSchema.statics = {
+    spawn: function (data, callback) {
+        Pregnant.create(mask(data, pregnantCreateMask),
+            (err, pregnant) => callback(err, mask(pregnant, pregnantMask)));
+    }
+};
+
+module.exports = Pregnant = mongoose.model('Pregnant', PregnantSchema);
