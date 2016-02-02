bf8caa7d29f888f80bac7ec95a6a60d504c9969d
Vinicius Teixeira <vinicius0026@gmail.com>
2016-02-02 21:34:53 -0200

    adjusted routes to families resource

--- client/app/families/familiesIndex.js
+++ client/app/families/familiesIndex.js
@@ -6,8 +6,18 @@
         $stateProvider
             .state('app.families', {
                 url: '/familias',
+                abstract: true,
+                template: '<div ui-view></div>'
+            })
+            .state('app.families.list', {
+                url: '/listar',
                 templateUrl: 'app/families/familiesTpl.html',
                 controller: 'FamiliesCtrl as vm'
+            })
+            .state('app.families.family', {
+                url: '/:action/:id',
+                templateUrl: 'app/families/familyTpl.html',
+                controller: 'FamilyCtrl as vm'
             });
     };
 
