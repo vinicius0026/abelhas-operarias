4cfcb916d099e335988c095cd9eb34c364a20903
Vinicius Teixeira <vinicius0026@gmail.com>
2016-02-02 23:10:03 -0200

    created familyCtrl

---
+++ client/app/families/familyCtrl.js
@@ -0,0 +1,107 @@
+(function () {
+    /* global angular */
+
+    'use strict';
+
+    var FamilyCtrl = function ($state, $stateParams, toastr,
+                                       familiesService) {
+        var vm = this,
+
+            validActions = ['cadastrar', 'visualizar', 'editar'],
+
+            validateAction = function (action) {
+                if (validActions.indexOf(action) < 0) {
+                    toastr.warning('Ação inválida');
+                    $state.go('app.families.list');
+                }
+            },
+
+            capitalizeFirstLetter = function (str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            },
+
+            createFamily = function (data) {
+                familiesService.create(data)
+                    .then(function () {
+                        toastr.success('Família cadastrada com sucesso.');
+                        $state.go('app.families.list');
+                    }, function (err) {
+                        if (/\$cpf_1 dup key/.test(err.data.info)) {
+                            return toastr.error('CPF já cadastrado no ' +
+                                'sistema.');
+                        }
+
+                        toastr.error('Erro ao cadastrar família. ' +
+                            'Favor tentar novamente');
+                    });
+            },
+
+            updateFamily = function (data, familyId) {
+                familiesService.update(familyId, data)
+                    .then(function () {
+                        toastr.success('Família atualizada com sucesso.');
+                        $state.go('app.families.family',
+                            {action: 'visualizar', id: vm.family.id},
+                            {reload: true});
+                    }, function () {
+                        toastr.error('Erro ao atualizar dados da gestante');
+                    });
+            },
+
+            saveFamily = function (familyData) {
+                if (vm.form.$valid) {
+                    if (vm.action === 'cadastrar') {
+                        createFamily(familyData);
+                    } else if (vm.action === 'editar') {
+                        updateFamily(familyData, $stateParams.id);
+                    }
+                } else {
+                    if (!vm.form.wifeName.$valid) {
+                        return toastr.warning('Preencha o nome da esposa.');
+                    }
+
+                    if (!vm.form.wifeCpf.$valid) {
+                        var msg = !vm.form.wifeCpf.$viewValue ? 'Preencha o CPF ' +
+                        'da esposa.' : 'O CPF informado é inválido.';
+                        return toastr.warning(msg);
+                    }
+
+                    toastr.warning('Preencha todos os campos obrigatórios.');
+                }
+            },
+
+            fetchFamily = function (familyId) {
+                if ($stateParams.action === 'editar' ||
+                    $stateParams.action === 'visualizar') {
+                    familiesService.get(familyId)
+                        .then(function (res) {
+                            vm.pregnant = res.data.data;
+                        }, function () {
+                            toastr.error('Erro ao ler dados da gestante');
+                        });
+                } else {
+                    vm.family = {};
+                }
+            },
+
+            editFamily = function () {
+                $state.go('app.families.family', {action: 'editar',
+                    id: vm.family.id}, {reload: true});
+            };
+
+        vm.capitalizeFirstLetter = capitalizeFirstLetter;
+        vm.action = $stateParams.action;
+        vm.savePregnant = saveFamily;
+        vm.editPregnant = editFamily;
+
+        validateAction(vm.action);
+        fetchFamily($stateParams.id);
+    };
+
+    FamilyCtrl.$inject = ['$state', '$stateParams', 'toastr',
+        'familiesService'];
+
+
+    angular.module('abelhas-operarias')
+        .controller('FamilyCtrl', FamilyCtrl);
+})();
