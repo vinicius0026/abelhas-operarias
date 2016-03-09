1a0d4ba7c8e00cc60f9c964d044b50c21a0445f6
Vinicius Teixeira <vinicius0026@gmail.com>
2016-03-09 00:16:27 -0300

    created Family API router file

---
+++ server/api/families/index.js
@@ -0,0 +1,27 @@
+/**
+ * Family API Router
+ */
+
+'use strict';
+
+var router = require('express').Router(),
+
+    auth = require('../../auth/authService'),
+    User = require('../users/userModel'),
+    controller = require('./familyController'),
+
+    userAuth = auth(User, true);
+
+router.post('/', userAuth.isAuthenticated(), controller.create);
+
+router.get('/:id', userAuth.isAuthenticated(),
+    controller.insertFamilyInRequest, controller.read);
+
+router.put('/:id', userAuth.isAuthenticated(),
+    controller.insertFamilyInRequest, controller.update);
+
+router.delete('/:id', userAuth.isAuthenticated(), controller.delete);
+
+router.post('/fetch', userAuth.isAuthenticated(), controller.fetch);
+
+module.exports = router;
