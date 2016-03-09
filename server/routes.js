825d0f966574d4d2888ba186e5837521c0ca7a15
Vinicius Teixeira <vinicius0026@gmail.com>
2016-03-08 23:34:47 -0300

    created route to families API

--- server/routes.js
+++ server/routes.js
@@ -7,5 +7,6 @@
 module.exports = function (app) {
     app.use('/auth', require('./auth'));
     app.use('/api/pregnant', require('./api/pregnant'));
+    app.use('/api/families', require('./api/families'));
     app.use('/api/users', require('./api/users'));
 };
