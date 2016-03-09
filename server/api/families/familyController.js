2639e00ed9fa8fd35eac808a64cb9fe4ddb2b0fb
Vinicius Teixeira <vinicius0026@gmail.com>
2016-03-09 00:16:54 -0300

    created controller for family API

---
+++ server/api/families/familyController.js
@@ -0,0 +1,102 @@
+/**
+ * Families API Controller
+ */
+
+'use strict';
+
+var datatablesQuery = require('datatables-query'),
+    errorHandler = require('../../lib/util/errorHandler'),
+
+    Family = require('./familyModel'),
+
+    familyController = {
+        create: function (req, res) {
+            var family = req.body;
+
+            Family.spawn(family, (err, family) => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                res.status(201).send({
+                    ok: true,
+                    info: 'Created family',
+                    data: family
+                });
+            });
+        },
+
+        read: function (req, res) {
+            var family = req.fiddus.family;
+
+            family.read((err, family) => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                res.send({
+                    ok: true,
+                    info: `Got family ${family.id}`,
+                    data: family
+                });
+            });
+        },
+
+        update: function (req, res) {
+            var family = req.fiddus.family,
+                updateData = req.body;
+
+            family.update(updateData, (err, family) => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                res.send({
+                    ok: true,
+                    info: `Updated family ${family.id}`,
+                    data: family
+                });
+            });
+        },
+
+        delete: function (req, res) {
+            var familyId = req.params.id;
+
+            Family.remove({_id: familyId}, err => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                res.sendStatus(204);
+            });
+        },
+
+        fetch: function (req, res) {
+            var params = req.body,
+                query = datatablesQuery(Family);
+
+            query.run(params).then(data => {
+                res.send(data);
+            }, err => {
+                errorHandler(err, res);
+            });
+
+        },
+
+        insertFamilyInRequest: function (req, res, next) {
+            var familyId = req.params.id;
+
+            req.fiddus = {};
+
+            Family.findById(familyId, (err, family) => {
+                if (err) {
+                    return errorHandler(err, res);
+                }
+
+                req.fiddus.family = family;
+                next();
+            });
+        }
+    };
+
+module.exports = familyController;
