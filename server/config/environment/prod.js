f0dc5f6ff260a8b6b15ee960795f2f4fab5ea600
Vinicius Teixeira <vinicius0026@gmail.com>
2020-08-29 19:03:16 -0300

    lint fix

--- server/config/environment/prod.js
+++ server/config/environment/prod.js
@@ -7,6 +7,8 @@
 module.exports = {
     db: {
         appDB: 'heroku_9jp74hcm',
-        baseUrl: `mongodb+srv://abelhasoperarias:${process.env.DB_PASSWORD}@cluster0.93ykz.mongodb.net/abelhas-operarias?retryWrites=true&w=majority`
+        baseUrl: `mongodb+srv://abelhasoperarias:${process.env.DB_PASSWORD}` +
+            `@cluster0.93ykz.mongodb.net/abelhas-operarias?retryWrites=true` +
+            `&w=majority`
     }
 };
