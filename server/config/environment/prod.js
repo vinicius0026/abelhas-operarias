6a0717b5d3b641260836db4f5739c9549ffaece1
Vinicius Teixeira <vinicius0026@gmail.com>
2020-08-29 19:20:52 -0300

    fix db conn string

--- server/config/environment/prod.js
+++ server/config/environment/prod.js
@@ -8,7 +8,7 @@ module.exports = {
     db: {
         appDB: 'heroku_9jp74hcm',
         baseUrl: `mongodb+srv://abelhasoperarias:${process.env.DB_PASSWORD}` +
-            `@cluster0.93ykz.mongodb.net/abelhas-operarias?retryWrites=true` +
+            `@cluster0.93ykz.mongodb.net/heroku_9jp74hcm?retryWrites=true` +
             `&w=majority`
     }
 };
